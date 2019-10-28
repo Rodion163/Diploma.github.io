24f555e812276df536f11c42bd4c410754e26f3e
Rodion163 <dronov.rodion@yandex.ru>
2019-10-28 19:55:00 +0300

    package

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,4 @@
 const path = require('path');
-<<<<<<< HEAD
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackMd5Hash = require('webpack-md5-hash');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // добавили плагин
-module.exports = {
-    entry: { main: './src/index.js' },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[chunkhash].js'
-    },
-=======
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
@@ -24,7 +13,6 @@ module.exports = {
         filename: 'main.js'
     },
     // указали путь к файлу, в квадратных скобках куда вставлять сгенерированный хеш
->>>>>>> master
     module: {
         rules: [
             {
@@ -35,10 +23,6 @@ module.exports = {
                 }
             },
             {
-<<<<<<< HEAD
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'] // добавили минификацию CSS
-=======
                 test: /\.(png|jpg|gif|ico|svg)$/,
                 use: [
                     'file-loader?name=../images/[name].[ext]', // указали папку, куда складывать изображения
@@ -62,18 +46,12 @@ module.exports = {
             {
                 test: /\.(eot|ttf|woff|woff2)$/,
                 loader: 'file-loader?name=./vendor/[name].[ext]'
->>>>>>> master
             }
         ]
     },
     plugins: [
-<<<<<<< HEAD
-        new MiniCssExtractPlugin({ // 
-            filename: 'style.[contenthash].css',
-=======
         new MiniCssExtractPlugin({
             filename: 'style.[contenthash].css'
->>>>>>> master
         }),
         new HtmlWebpackPlugin({
             inject: false,
@@ -81,9 +59,6 @@ module.exports = {
             template: './src/index.html',
             filename: 'index.html'
         }),
-<<<<<<< HEAD
-        new WebpackMd5Hash()
-=======
         new WebpackMd5Hash(),
         new OptimizeCssAssetsPlugin({
             assetNameRegExp: /\.css$/g,
@@ -96,6 +71,5 @@ module.exports = {
         new webpack.DefinePlugin({
             'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
         })
->>>>>>> master
     ]
 };
