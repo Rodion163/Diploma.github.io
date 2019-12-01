317277fee59bfdeeef96227168f40c700abe36d4
Roidon163 <dronov.rodion@yandex.ru>
2019-12-01 22:14:58 +0300

    config glider

--- src/about.js
+++ src/about.js
@@ -1,4 +1,33 @@
 import "./styles/about.css";
 import Glide from '@glidejs/glide'
 
-new Glide('.slider').mount()
+new Glide('.slider', {
+    perView: 3,
+    peek: {
+        before: 88,
+        after: 88
+    },
+    classes: {
+        swipeable: 'glide--swipeable',
+        dragging: 'glide--dragging',
+        direction: {
+          ltr: 'glide--ltr',
+          rtl: 'glide--rtl'
+        },
+        type: {
+          slider: 'glide--slider',
+          carousel: 'glide--carousel'
+        },
+        slide: {
+          clone: 'glide__slide--clone',
+          active: 'glide__slide--active'
+        },
+        arrow: {
+          disabled: 'glide__arrow--disabled'
+        },
+        nav: {
+          active: 'pagination__point_active'
+        }
+      }
+}).mount()
+
